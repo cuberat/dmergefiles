0795a9817ccad49f4c56b889f65c392db8d61e27
Don Owens <don@regexguy.com>
2018-05-13 00:27:32 +0100

    added support for (de)compression

--- dmergefiles.go
+++ dmergefiles.go
@@ -1,11 +1,87 @@
-package main
+// Copyright (c) 2018 Don Owens <don@regexguy.com>.  All rights reserved.
+//
+// This software is released under the BSD license:
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//  * Neither the name of the author nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Currently assumes files are in sorted order
+// The dmergefiles program merges columns in tab-delimited files for the
+// same "key" in the first column of each file. It is assumed that the
+// input files are pre-sorted asciibetically (if using unix sort, set env
+// LC_ALL="C").
+//
+// Columns from each file are added in the order the files are
+// specified. E.g., with input from two files:
+//
+// file1:
+//    bar	11	22	33
+//    cat	01	02	03
+//    foo	1	2	3
+//
+// file2:
+//    bar	44	55	66
+//    car	7	8	9
+//    foo	4	5	6
+//
+// The output is
+//    bar	11	22	33	44	55	66
+//    car				7	8	9
+//    cat	01	02	03			
+//    foo	1	2	3	4	5	6
+//
+// Installation:
+//
+// To get the latest changes
+//
+//     go get github.com/cuberat/dmergefiles
+//
+// Usage
+//    Usage: ./dmergefiles [options] files ...
+//
+//    Options:
+//
+//    -outfile string
+//        Output file
+//    -stdout
+//        Write to standard output
+//    -v    Be verbose
+//
+// Input and output files are supported with (de)compression, based on the file
+// name extension. Supported
+// (de)compression: gzip, bzip2, xz.
+package main
 
 import (
     "bufio"
     "flag"
     "fmt"
+    "github.com/cuberat/go-libutils/libutils"
     "io"
     "os"
     "strings"
@@ -20,7 +96,7 @@ type Context struct {
 type FileSpec struct {
     path string
     reader *bufio.Reader
-    orig_fp *os.File
+    closer_func libutils.CloseFunc
     num_cols int
     last_host string
     last_cols []string
@@ -62,12 +138,13 @@ func main() {
     if to_stdout {
         ctx.writer = os.Stdout
     } else {
-        out_fh, err := os.Create(out_file)
+        out_fh, closer_func, err := libutils.OpenFileW(out_file)
         if err != nil {
             fmt.Fprintf(os.Stderr, "Couldn't open file %s for output.", out_file)
             os.Exit(-1)
         }
-        defer out_fh.Close()
+
+        defer closer_func()
 
         ctx.writer = out_fh
     }
@@ -91,7 +168,7 @@ func process_files(ctx *Context, file_paths []string) error {
 
     defer func() {
         for _, file := range files {
-            file.orig_fp.Close()
+            file.closer_func()
         }
     }()
 
@@ -185,7 +262,7 @@ func open_files(ctx *Context, file_paths []string) ([]*FileSpec, error) {
     files := make([]*FileSpec, 0, len(file_paths))
     
     for _, path := range file_paths {
-        fp, err := os.Open(path)
+        r, closer_func, err := libutils.OpenFileRO(path)
         if err != nil {
             return files, fmt.Errorf("couldn't open file %s for input: %s", path, err)
         }
@@ -194,8 +271,8 @@ func open_files(ctx *Context, file_paths []string) ([]*FileSpec, error) {
 
         spec := new(FileSpec)
         spec.path = path
-        spec.orig_fp = fp
-        spec.reader = bufio.NewReader(fp)
+        spec.closer_func = closer_func
+        spec.reader = bufio.NewReader(r)
 
         files = append(files, spec)
     }
